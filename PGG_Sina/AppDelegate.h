5b9949fc75a8096ccd072247a27892ff13642287
penghero <896733185@qq.com>
2017-12-13 09:12:46 +0800

    Initial Commit

---
+++ PGG_Sina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGG_Sina
+//
+//  Created by 陈鹏 on 2017/12/13.
+//  Copyright © 2017年 penggege.CP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
