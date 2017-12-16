7cdf80062f3b75550be90917e120906c75a385e3
penghero <896733185@qq.com>
2017-12-16 10:45:23 +0800

    更新
    让自定义按钮超出TabBar部分后  处理超出部分的不响应事件

--- PGG_Sina/MainTabAndNav/PGGTabBar/PGGAddTabBar.m
+++ PGG_Sina/MainTabAndNav/PGGTabBar/PGGAddTabBar.m
@@ -44,9 +44,9 @@
         //设置加号的位置
     self.plusBtn.centerX = self.width *0.5;
     if (kDevice_Is_iPhoneX) {
-        self.plusBtn.centerY = self.height *0.3;
+        self.plusBtn.centerY = self.height *0.3; //没有超过tabbar高度 无需处理
     } else {
-        self.plusBtn.centerY = self.height *0.5;
+        self.plusBtn.centerY = self.height *0.05;//加号超过tabbar高度后 无响应
     }
         //设置其他tabbarButton的位置和尺寸
     CGFloat tabBarButtonW  = self.width / 5;
@@ -64,6 +64,30 @@
         }
     }
 }
+
+#pragma mark - 重写hitTest方法 处理凸出的部分点击无效
+ - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+/*这一个判断是关键，不判断的话push到其他页面，点击自定义按钮的位置也是会有反应的
+ *self.isHidden == NO 说明当前页面是有tabbar的，那么肯定是在导航控制器的根控制器页面
+ *在导航控制器根控制器页面，那么我们就需要判断手指点击的位置是否在自定义按钮身上
+ *是的话让自定义按钮自己处理点击事件，不是的话让系统去处理点击事件就可以了
+ */
+     if (self.isHidden == NO) {
+         /* 将当前tabbar的触摸点转换坐标系，转换到发布按钮的身上，生成一个新的点 */
+         CGPoint newPoint = [self convertPoint:point toView:self.plusBtn];
+         /* 判断如果这个新的点是在自定义按钮身上，处理突出部分的自定义按钮*/
+         if ( [self.plusBtn pointInside:newPoint withEvent:event]) {
+             return self.plusBtn;
+         }else{
+             /*如果点不在自定义按钮身上，直接让系统处理*/
+             return [super hitTest:point withEvent:event];
+         }
+     }
+     else {
+         /* tabbar隐藏后，那么说明已经push到其他的页面了，这个时候还是让系统去判断最合适的view处理*/
+         return [super hitTest:point withEvent:event];
+     }
+  }
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
